d2e7aa7952508896dfe3181c2dbcd313e67803e9
ahmedl <ahmedl@P0013076>
2024-09-20 09:04:54 +0400

    init

---
+++ NETOAuthApp.MVC/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace NETOAuthApp.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet("~/login")]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            string? redirectUrl = Url.Action(nameof(LoginCallback), "Account", new { returnUrl });
+            AuthenticationProperties properties = new () { RedirectUri = redirectUrl };
+            return Challenge(properties, "GitHub");
+        }
+
+        [HttpGet("~/signin-github")]
+        public async Task<IActionResult> LoginCallback()
+        {
+            AuthenticateResult authenticateResult = await HttpContext.AuthenticateAsync("GitHub");
+
+            if (!authenticateResult.Succeeded)
+                return BadRequest();
+
+            IEnumerable<Claim> claims = authenticateResult.Principal.Claims;
+            return View(claims);
+        }
+
+        [HttpGet("~/logout")]
+        public IActionResult Logout()
+        {
+            return SignOut("GitHub", "Cookies");
+        }
+    }
+}
--- NETOAuthApp.MVC/Program.cs
+++ NETOAuthApp.MVC/Program.cs
@@ -1,15 +1,57 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
 
-// Add services to the container.
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "GitHub";
+    options.DefaultChallengeScheme = "GitHub";
+})
+.AddOAuth("GitHub", options =>
+{
+    options.ClientId = "Ov23liNiqZ4HgelGOmW2";
+    options.ClientSecret = "2b03b9afdd08e00c47ba31e8c8bcace02cccb407";
+    options.CallbackPath = new PathString("/signin-github");
 
-// Configure the HTTP request pipeline.
+    options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+    options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+    options.UserInformationEndpoint = "https://api.github.com/user";
+
+    // Define the data we want from the user
+    options.Scope.Add("user:email");
+
+    options.SaveTokens = true;
+
+    // Event handler to get user info from GitHub after authentication
+    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
+    options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+
+    options.Events.OnCreatingTicket = async context =>
+    {
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, options.UserInformationEndpoint);
+        
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        HttpResponseMessage response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+        response.EnsureSuccessStatusCode();
+
+        JsonDocument user = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+        context.RunClaimActions(user.RootElement);
+    };
+});
+
+WebApplication app = builder.Build();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,6 +60,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
